9d7641fed96860a4bd775288e9ea16ce6cc74eb3
fase53728-cell <fase53728@gmail.com>
2025-11-18 19:56:10 -0300

    Delete backend directory

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-CSV_DIR = os.path.join(BASE_DIR, "csv")
-if not os.path.exists(CSV_DIR):
-    os.makedirs(CSV_DIR)
--- backend/models.py
+++
@@ -1,7 +0,0 @@
-class TeamStats:
-    def __init__(self, name, data):
-        self.name = name
-        self.data = data
-
-    def get_stat(self, key, default=None):
-        return self.data.get(key, default)
