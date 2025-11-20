962f5069b6e698665fc765c2f9bd4f7ee3870da3
fase53728-cell <fase53728@gmail.com>
2025-11-20 20:11:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template
-from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
-# -----------------------------
-# ROTA PRINCIPAL DO PAINEL
-# -----------------------------
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# -----------------------------
-# RODAR SERVIDOR NO RENDER
-# -----------------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
